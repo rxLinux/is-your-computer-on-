e09c2f58e1a7348ea9ffb92de93c310b2e5b84f8
rxLinux <ruyjorge2025@proton.me>
2025-11-20 22:33:15 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Minha GUI")
+root.geometry("1000x1000")
+
+label = tk.Label(root, text="sim", font=("Arial", 12))
+label.pack(expand=True)
+
+root.mainloop()s
